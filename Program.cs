61f92fd9ca236bfa6e2fee209cda1e4601af40b9
Evgeniy Pikin <pikin@bk.ru>
2023-04-29 11:47:25 +0500

    Task 47 added

--- Program.cs
+++ Program.cs
@@ -5,27 +5,45 @@
 // 8 7,8 -7,1 9
 
 
+double [,] GetArray(int num, int column)
+{
+    double [,] arr = new double[num,column];
+    return arr;
+}
+
 double GetRandom()
 {
     return Math.Round(new Random().NextDouble(), 1) + new Random().Next(-10, 11);
 }
 
-void FillArray(double [,] arr)
+double [,] FillArray(double [,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
         for (int j = 0; j < arr.GetLength(1); j++)
         {
             arr[i,j] = GetRandom();
-            Console.Write(arr[i,j] + "   ");
         }
-    Console.WriteLine();    
+        
     }
+    return arr;
 }
 
-double [,] array = new double[3,4];
-FillArray(array);
+void PrintArray(double [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i,j] + "    ");
+        }
+    Console.WriteLine(); 
+    }   
+}
 
+double [,] array = GetArray(3, 4);
+FillArray(array);
+PrintArray(array);
 
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
