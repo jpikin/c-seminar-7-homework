0d6fdb660fbfbb3eaa8cd60e61be6bd1a6437bf2
Evgeniy Pikin <pikin@bk.ru>
2023-05-01 03:09:05 +0500

    Task 52 just for fun added

--- Program.cs
+++ Program.cs
@@ -54,28 +54,28 @@
 // 8 4 2 4
 // 17 -> такого числа в массиве нет
 
-int[] GetNums()
-{
-    Console.WriteLine("Введите через запятую позицию элемента в духмерном массиве");
-    int [] num =  Array.ConvertAll<string,int>(Console.ReadLine().Split(','), 
-    elem => { return Convert.ToInt32 (elem);});
-    return num;
-}
+// int[] GetNums()
+// {
+//     Console.WriteLine("Введите через запятую позицию элемента в духмерном массиве");
+//     int [] num =  Array.ConvertAll<string,int>(Console.ReadLine().Split(','), 
+//     elem => { return Convert.ToInt32 (elem);});
+//     return num;
+// }
 
-string FindNum(int[,] arr, int [] num)
-{
-    if (num[0] < arr.GetLength(0) && num[1] < arr.GetLength(1))
-    {
-        return Convert.ToString(arr[num[0], num[1]]);
-    }
-    else return "Такого элемента в массиве нет";      
-}
+// string FindNum(int[,] arr, int [] num)
+// {
+//     if (num[0] < arr.GetLength(0) && num[1] < arr.GetLength(1))
+//     {
+//         return Convert.ToString(arr[num[0], num[1]]);
+//     }
+//     else return "Такого элемента в массиве нет";      
+// }
 
-int [,] array = {{1,4,7,2},
-                 {5,9,2,3},
-                 {8,4,2,4}};
+// int [,] array = {{1,4,7,2},
+//                  {5,9,2,3},
+//                  {8,4,2,4}};
 
-Console.WriteLine(FindNum(array, GetNums()));
+// Console.WriteLine(FindNum(array, GetNums()));
 
 
 // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
@@ -86,8 +86,8 @@ Console.WriteLine(FindNum(array, GetNums()));
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
 // int [,] array = {{1,4,7,2},
-//                  {5,9,2,3},
-//                  {8,4,2,4}};
+//                   {5,9,2,3},
+//                   {8,4,2,4}};
 // double [] result = new double[array.GetLength(1)];
 
 // for (int i = 0; i < array.GetLength(1); i++)
@@ -101,3 +101,49 @@ Console.WriteLine(FindNum(array, GetNums()));
 // }
 
 // Console.WriteLine($"Среднее арифметическое каждого столбца: {String.Join("; ", result)}");
+
+// *****************************************************************
+// int i = 0, j = 0;
+// double sum0 = 0, sum1 = 0, sum2 = 0, sum3 = 0;
+// for (int k = 0; k < (array.GetLength(0)*array.GetLength(1)); k++)
+// {
+//         switch (j)
+//         {
+//             case 0:
+//             {
+//                 sum0 += array[i,j];
+//                 j++;
+//                 break;
+//             }
+//             case 1:
+//             {
+//                 sum1 += array[i,j];
+//                 j++;
+//                 break;
+//             }
+//             case 2:
+//             {
+//                 sum2 += array[i,j];
+//                 j++;
+//                 break;
+//             }
+//             case 3:
+//             {
+//                 sum3 += array[i,j];
+//                 j++;
+//                 break;
+//             }
+//             case 4:
+//             {
+                
+//                 j = 0;
+//                 i++;
+//                 break;
+//             }
+//         }    
+   
+// }
+// Console.WriteLine(sum0/4);
+// Console.WriteLine(sum1/4);
+// Console.WriteLine(sum2/4);
+// Console.WriteLine(sum3/4);
